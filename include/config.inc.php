4e07dbfe59cfd65bd16d912757774793b7ba2c65
Mathew Sprehn <matsprehn@gmail.com>
2013-02-20 16:22:48 -0800

    removed config from the gitignore. Should work now

---
+++ include/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+define('PAGE_TITLE', 'The Harvest Club');
+define('PAGE_QUOTE', 'Share the Bounty');
+
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'theha47_cpanel');
+define('MYSQL_PASS', '');
+define('MYSQL_DB', 'theha47_cpanel');
+
+define('MAIL_FROM', 'harvesting@theharvestclub.org');
+define('MAIL_TO', 'ssalat@uci.edu');
+define('MAIL_REPLYTO', 'harvesting@theharvestclub.org');
+
+define('SESSION_MAX_LENGTH', 999999999); // logout after inactive for x seconds
+
+
+?>
