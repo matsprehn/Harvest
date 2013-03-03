c4c55fe6d8ac5977f912a98af78edc9f3578b2a3
App Jam <appjam@Air06.local>
2013-03-02 21:38:10 -0800

    Pulls the upcomming events
    The page still doesn't link to the database though. Also remember to
    create events that are comming up in the future or nothing will be
    displayed because our database is out of date.

---
+++ RSVPForm.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!-- saved from url=(0085)https://docs.google.com/forms/d/1zvS8beJH6EeSxzlPGBR8R0xLPFRkkaCfou0DCPqxQ-o/viewform -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="shortcut icon" href="https://ssl.gstatic.com/docs/spreadsheets/forms/favicon_jfk2.png" type="image/x-icon">
+
+<meta http-equiv="X-UA-Compatible" content="IE=10; chrome=1;">
+<meta name="fragment" content="!">
+<!--<base target="_blank">--><base href="." target="_blank">
+<title>Harvest Event RSVP Form</title>
+
+<link href="./Harvest Event RSVP Form_files/3227177689-formview_ltr.css" type="text/css" rel="stylesheet">
+<style type="text/css">
+
+body {
+  background: #fffbef url('//themes.googleusercontent.com/image?id=1a0AeDLTHbl0veIcjLG7wyMEOtSe8L5jsDMsuV_d1_xFhDH5Ki5qyGKxBPm30vy7v3vIh');
+  color: #28642e;
+  font-family: Molengo,'Trebuchet MS',Tahoma,Verdana,Arial,sans-serif;
+  padding: 0;
+}
+input, select, textarea {
+  font-family: Tahoma,Arial,serif;
+  font-size: 13px;
+}
+a {
+  color: #34798d;
+}
+div.ss-form-container {
+  background: #fffbef;
+  border: 1px solid #28642e;
+  font-size: 14px;
+  line-height: 18px;
+  margin: 0 auto;
+  max-width: 920px;
+  padding: 20px;
+  width: 80%;
+}
+div.ss-form-heading {
+  margin: 0 0 20px;
+}
+h1, h2 {
+  font-family: 'Josefin Sans Std Light';
+  font-weight: normal;
+}
+h1.ss-form-title {
+  font-size: 36px;
+  line-height: 40px;
+  text-align: center;
+}
+.ss-required-asterisk {
+  color: #bc3611;
+}
+div.errorbox-bad {
+  background: none;
+  border: none; 
+  padding: 0;
+}
+div.errorheader, div.errorbox-bad .ss-form-entry {
+  background: none;
+  border: 2px solid #bc3611;
+  border-radius: 5px;
+  color: #bc3611;
+  padding: 5px;
+}
+div.ss-section-header, div.ss-page-break {
+  margin: 15px 0;
+  padding: 0;
+}
+h2.ss-section-title, h2.ss-page-title {
+  background: none;
+  border-bottom: 1px dashed #abc84d;
+  font-size: 24px;
+  line-height: 28px;
+  margin: 0 0 5px;
+  padding: 0 0 5px;
+}
+td.ss-scalenumbers, td.ss-scalerow, td.ss-gridnumbers, td.ss-gridrow, tr.ss-grid-row-odd, tr.ss-grid-row-even {
+  border-color: #abc84d;
+}
+tr.ss-grid-row-odd {
+  background: #fbffef;
+}
+tr.ss-grid-row-even {
+  background: #fffbef;
+}
+label.ss-q-help {
+  color: #999;
+}
+td.ss-scalenumbers, td.ss-scalerow, td.ss-leftlabel, td.ss-rightlabel, label.ss-gridnumber, span.ss-powered-by, .ss-powered-by a:link, .ss-powered-by a:visited, span.ss-terms {
+  color: #28642e;
+}
+
+</style>
+<meta name="viewport" content="width=device-width">
+
+<link href="./Harvest Event RSVP Form_files/195262907-mobile_formview_ltr.css" type="text/css" rel="stylesheet" media="screen and (max-device-width: 600px)"><style type="text/css"></style></head>
+<body dir="ltr" class="ss-base-body"><div itemscope="" itemtype="http://schema.org/CreativeWork/FormObject"><meta itemprop="name" content="Harvest Event RSVP Form">
+<meta itemprop="description" content="Please RSVP for the Harvest Club upcoming events by filling out the form. If you have any questions please contact us at volunteer@theharvestclub.org.
+
+Events last 1-2.5 hours.">
+
+<meta itemprop="url" content="https://docs.google.com/forms/d/1zvS8beJH6EeSxzlPGBR8R0xLPFRkkaCfou0DCPqxQ-o/viewform">
+<meta itemprop="embedUrl" content="https://docs.google.com/forms/d/1zvS8beJH6EeSxzlPGBR8R0xLPFRkkaCfou0DCPqxQ-o/viewform?embedded=true">
+<meta itemprop="faviconUrl" content="https://ssl.gstatic.com/docs/spreadsheets/forms/favicon_jfk2.png">
+
+
+<div class="ss-form-container"><div class="ss-top-of-page">
+
+<div class="ss-form-heading"><h1 class="ss-form-title" dir="ltr">Harvest Event RSVP Form</h1>
+<p></p>
+<div class="ss-form-desc ss-no-ignore-whitespace">Please RSVP for the Harvest Club upcoming events by filling out the form. If you have any questions please contact us at <a href="mailto:volunteer@theharvestclub.org">volunteer@theharvestclub.org</a>.<p>Events last 1-2.5 hours.</p></div>
+
+<hr class="ss-email-break" style="display:none;">
+<div class="ss-required-asterisk">* Required</div></div></div>
+<div class="ss-form"><form action="https://docs.google.com/forms/d/1zvS8beJH6EeSxzlPGBR8R0xLPFRkkaCfou0DCPqxQ-o/formResponse" method="POST" id="ss-form" target="_self" onsubmit="">
+<div class="errorbox-good">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_742559633"><div class="ss-q-title">Last Name
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.742559633" value="" class="ss-q-short" id="entry_742559633" dir="auto" aria-required="true">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1921962798"><div class="ss-q-title">First Name
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.1921962798" value="" class="ss-q-short" id="entry_1921962798" dir="auto" aria-required="true">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1990658666"><div class="ss-q-title">E-mail
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.1990658666" value="" class="ss-q-short" id="entry_1990658666" dir="auto" aria-required="true">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_191998386"><div class="ss-q-title">Phone
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.191998386" value="" class="ss-q-short" id="entry_191998386" dir="auto" aria-required="true">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item ss-item-required ss-radio"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_111406964"><div class="ss-q-title">Choose Event Date
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<?php   
+require_once('include/auth.inc.php');
+require_once('include/Database.inc.php');
+
+$sql = "SELECT Distinct growers.city, tree_types.name, events.date, events.time
+FROM `events` 
+	join growers on events.grower_id = growers.id 
+        join grower_trees on grower_trees.grower_id = growers.id 
+        join tree_types on grower_trees.tree_type = tree_types.id
+WHERE events.date >= curDate() and growers.id = events.grower_id
+order by events.date";
+//echo $sql;
+
+$results = $db->q($sql);
+echo "</br>";
+//echo "</br>";
+if($results === FALSE) {
+    die(mysql_error()); // TODO: better error handling
+}
+
+$checkcity=null;
+$checkdate=null;
+$checktime=null;
+$checkname=null;
+$visited = false;
+// Need to use a method from the Results class, can be found in Database.inc.php under Results
+$event = array();
+$all_Events = array();
+while ($row = $results->getAssoc()) {
+  $city = $row['city'];
+  $name = $row['name'];
+  $date = $row['date'];
+  $time = $row['time'];
+  
+
+  array_push($event, $city, $name, $date, $time);
+  array_push($all_Events, $event);
+}
+//echo "count is ". $count1;
+
+$count = count($all_Events);
+$count1 = count($all_Events[$count-1]);
+//echo "count is ". $count1;
+$all_Events[$count-1];
+$all = $all_Events[$count-1];
+
+
+$x = 0;
+while(!empty($all))
+{
+  $city = $all[$x];
+  $x++;
+  $type = $all[$x];
+  $x++;
+  $date = $all[$x];
+  $x++;
+  $time = $all[$x];
+  $x++;
+  
+  $x = 0;
+  
+$temp1 = array_shift($all);
+$temp2 = array_shift($all);
+$temp3 = array_shift($all);
+$temp4 = array_shift($all);
+
+if(!in_array($city, $all)){ 
+   echo "<form> <input type=\"radio\" name=\"sex\" value=\"male\">".$name." Harvest in ".$city." [ ".$date." , ".$time." ] "."<br> </form>"; // Haven'st added the full info yet
+   echo "</br>";
+$name=null;
+}
+
+if(in_array($city, $all) && in_array($date, $all) && in_array($time, $all))
+{
+  //echo "there is a value that is equal";
+  $name = $name." and ".$all[$x+1];
+  //echo "</br>";
+}
+else{
+  //echo "This is the only event";
+  $name = $all[$x+1];
+}
+}
+?>
+</br>
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item  ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_186780307"><div class="ss-q-title">First Name of Guest 1
+</div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.186780307" value="" class="ss-q-short" id="entry_186780307" dir="auto">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item  ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_182815174"><div class="ss-q-title">Last Name of Guest 1
+</div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.182815174" value="" class="ss-q-short" id="entry_182815174" dir="auto">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item  ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_236939303"><div class="ss-q-title">Email of Guest 1
+</div>
+<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<input type="text" name="entry.236939303" value="" class="ss-q-short" id="entry_236939303" dir="auto">
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item  ss-section-header"><div class="ss-form-entry"><h2 class="ss-section-title">Waiver Form</h2>
+<div class="ss-section-description ss-no-ignore-whitespace">Please read the following waiver and sign electronically below:
+<a href="http://goo.gl/BmxHG">http://goo.gl/BmxHG</a>
+
+</div>
+</div></div></div> <div class="errorbox-good">
+<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_2023792896"><div class="ss-q-title">I agree to the terms and conditions
+<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+<span class="ss-required-asterisk">*</span></div>
+<div class="ss-q-help ss-secondary-text" dir="ltr">Sign electronically with your full name. </div></label>
+<input type="text" name="entry.2023792896" value="" class="ss-q-short" id="entry_2023792896" dir="auto" aria-required="true">
+</div></div></div>
+<input type="hidden" name="draftResponse" value="[]
+">
+<input type="hidden" name="pageHistory" value="0">
+
+
+<div class="ss-item ss-navigate"><div class="ss-form-entry">
+<input type="submit" name="submit" value="Submit" id="ss-submit">
+<div class="ss-secondary-text"></div></div></div></form></div>
+<div class="ss-footer"><div class="ss-attribution"></div>
+<div class="ss-legal"><div class="disclaimer-separator"></div>
+<div class="disclaimer"><div class="powered-by-logo"><span class="powered-by-text"></span>
+</div>
+<div class="ss-terms"><span class="disclaimer-msg"></span>
+<br>
+</div></div></div></div>
+</div>
+
+<script type="text/javascript" src="./Harvest Event RSVP Form_files/3090771720-formviewer_prd.js"></script>
+<script type="text/javascript">_initFormViewer();</script></div></body></html>
