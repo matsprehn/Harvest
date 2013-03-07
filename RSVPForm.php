21c2883943b8332d220a33b72b75b95f21fb953a
App Jam <appjam@dhcp-v008-177.mobile.uci.edu>
2013-03-07 14:33:52 -0800

    form is now "readable"

--- RSVPForm.php
+++ RSVPForm.php
@@ -7,93 +7,82 @@
 <!--<base target="_blank">--><base href="." target="_blank">
 <title>Harvest Event RSVP Form</title>
 
-<link href="./Harvest Event RSVP Form_files/3227177689-formview_ltr.css" type="text/css" rel="stylesheet">
 <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="js/addingGuests.js"></script>
 <script type="text/javascript" src="js/validate.js"></script>
 <link href="css/rsvpForm.css" rel="stylesheet" type="text/css" />
 
+<div class="form-container">
 
-<script type="text/javascript">
-$().ready(function() {
-  // validate the comment form when it is submitted
-  alert("yolo");
-  $("#ss-form").validate();
-
-/*
-  // validate signup form on keyup and submit
-  $("#ss-form").validate({
-    rules: {
-      entry_742559633: "required",
-      entry_1921962798: "required",
-    },
-    messages: {
-      entry_742559633: "Please enter your firstname",
-      entry_1921962798: "Please enter your lastname",
-    }
-  });
-  $("#ss-form").validate();
-*/
-});
-</script>
-
-
-
-
-<meta name="viewport" content="width=device-width">
-
-<link href="./Harvest Event RSVP Form_files/195262907-mobile_formview_ltr.css" type="text/css" rel="stylesheet" media="screen and (max-device-width: 600px)"><style type="text/css"></style></head>
-<body dir="ltr" class="ss-base-body"><div itemscope="" itemtype="http://schema.org/CreativeWork/FormObject"><meta itemprop="name" content="Harvest Event RSVP Form">
-<meta itemprop="description" content="Please RSVP for the Harvest Club upcoming events by filling out the form. If you have any questions please contact us at volunteer@theharvestclub.org.
-
-Events last 1-2.5 hours.">
-
-<meta itemprop="url" content="https://docs.google.com/forms/d/1zvS8beJH6EeSxzlPGBR8R0xLPFRkkaCfou0DCPqxQ-o/viewform">
-<meta itemprop="embedUrl" content="https://docs.google.com/forms/d/1zvS8beJH6EeSxzlPGBR8R0xLPFRkkaCfou0DCPqxQ-o/viewform?embedded=true">
-<meta itemprop="faviconUrl" content="https://ssl.gstatic.com/docs/spreadsheets/forms/favicon_jfk2.png">
-
-
-<div class="ss-form-container"><div class="ss-top-of-page">
-
-<div class="ss-form-heading"><h1 class="ss-form-title" dir="ltr">Harvest Event RSVP Form</h1>
+<div class="form-heading"><h1 class="form-title" dir="ltr">Harvest Event RSVP Form</h1>
 <p></p>
 <div class="ss-form-desc ss-no-ignore-whitespace">Please RSVP for the Harvest Club upcoming events by filling out the form. If you have any questions please contact us at <a href="mailto:volunteer@theharvestclub.org">volunteer@theharvestclub.org</a>.</div>
 
-<hr class="ss-email-break" style="display:none;">
-<div class="ss-required-asterisk">* Required</div></div></div>
-<div class="ss-form">
-<form  class="cmxform" action="thankyou.php" method="POST" id="ss-form">
+<div class="">* Required</div>
+</div>
+<div class="form">
+	<form  class="cmxform" action="thankyou.php" method="POST" id="ss-form">
+		<div class="errorbox-good">
+			<div>
+				<div class="form-entry">
+					<label>
+						<div> First Name
+							<span class="ss-required-asterisk">*</span>
+						</div>
+					</label>
+						<input type="text" name="name" class="ss-q-short" id="fname"> 
+				</div>
+			</div>
+		</div> 
+
 <div class="errorbox-good">
-<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_742559633"><div class="ss-q-title">First Name
-<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
-<span class="ss-required-asterisk">*</span></div>
-<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+			<div>
+				<div class="form-entry">
+					<label>
+						<div> Last Name
+							<span class="ss-required-asterisk">*</span>
+						</div>
+					</label>
+						<input type="text" name="lname" class="ss-q-short" id="lname" dir="auto"> 
+				</div>
+			</div>
+		</div> 
 
-<input type="text" name="entry.742559633" value="" class="ss-q-short required " id="entry_742559633" dir="auto">
+<div class="errorbox-good">
+			<div>
+				<div class="form-entry">
+					<label>
+						<div> E-mail
+							<span class="ss-required-asterisk">*</span>
+						</div>
+					</label>
+						<input type="text" name="email" class="ss-q-short" id="email" dir="auto"> 
+				</div>
+			</div>
+		</div> 
 
-</div></div></div> <div class="errorbox-good">
-<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1921962798"><div class="ss-q-title">Last Name
-<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
-<span class="ss-required-asterisk">*</span></div>
-<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
-<input type="text" name="entry.1921962798" value="" class="ss-q-short" id="entry_1921962798" class="required" dir="auto" aria-required="true">
-</div></div></div> <div class="errorbox-good">
-<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_1990658666"><div class="ss-q-title">E-mail
-<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
-<span class="ss-required-asterisk">*</span></div>
-<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
-<input type="text" name="entry.1990658666" value="" class="ss-q-short" id="entry_1990658666" dir="auto" aria-required="true">
-</div></div></div> <div class="errorbox-good">
-<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_191998386"><div class="ss-q-title">Phone
-<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
-<span class="ss-required-asterisk">*</span></div>
-<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
-<input type="text" name="entry.191998386" value="" class="ss-q-short" id="entry_191998386" dir="auto" aria-required="true">
-</div></div></div><br/><div class="errorbox-good">
-<div dir="ltr" class="ss-item ss-item-required ss-radio"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_111406964"><div class="ss-q-title">Choose any Event that you want to participate in.</br> <p>Events typically last 1-2.5 hours.</p>
-<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
-<span class="ss-required-asterisk"></span></div>
-<div class="ss-q-help ss-secondary-text" dir="ltr"></div></label>
+<div class="errorbox-good">
+			<div>
+				<div class="form-entry">
+					<label>
+						<div> Phone Number
+							<span class="ss-required-asterisk">*</span>
+						</div>
+					</label>
+						<input type="text" name="phone" class="ss-q-short" id="phone" dir="auto"> 
+				</div>
+			</div>
+		</div> 
+
+<br/>
+<div class="errorbox-good">
+	<div>
+		<div class="form-entry">
+			<label>
+				<div>Choose any Event that you want to participate in.</br> <p>Events typically last 1-2.5 hours.</p>
+					<span class="ss-required-asterisk"></span>
+				</div>
+			</label>
 <?php   
 require_once('include/auth.inc.php');
 require_once('include/Database.inc.php');
@@ -217,37 +206,48 @@ if(isset( $all[0])){
 <br>
 <input type="checkbox" id="waiver_box"> This is my first harvest! </br>
 <div class ="waiver">
-<div dir="ltr" class="ss-item  ss-section-header"><div class="ss-form-entry"><h2 class="ss-section-title">Waiver Form</h2>
-<div class="ss-section-description ss-no-ignore-whitespace">Please read the following waiver and sign electronically below:
-<a href="2012 THC Liability Form and Photo Release copy.pdf">http://goo.gl/BmxHG</a>
+	<div>
+		<div class="form-entry"><h2 class="section-title">Waiver Form</h2>
+			<div class="section-description no-ignore-whitespace">Please read the following waiver and sign electronically below:
+				<a href="2012 THC Liability Form and Photo Release copy.pdf">http://goo.gl/BmxHG</a>
 
+			</div>
+		</div>
+	</div>
 </div>
-</div></div></div></div> <div class ="waiver"><div class="errorbox-good">
-<input type="checkbox" id="waiver_box"> I allow my picture to be taken</br>
-</br>
-<div dir="ltr" class="ss-item ss-item-required ss-text"><div class="ss-form-entry"><label class="ss-q-item-label" for="entry_2023792896"><div class="ss-q-title">I agree to the terms and conditions
-<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
-<span class="ss-required-asterisk">*</span></div>
-<div class="ss-q-help ss-secondary-text" dir="ltr">Sign electronically with your full name. </div></label>
-<input type="text" name="entry.2023792896" value="" class="ss-q-short" id="entry_2023792896" dir="auto" aria-required="true">
-</div></div></div></div>
-<input type="hidden" name="draftResponse" value="[]
-">
+</div> 
+
+<div class ="waiver">
+	<div class="errorbox-good">
+		<input type="checkbox" id="waiver_box"> I allow my picture to be taken</br>
+			</br>
+				<div>
+					<div class="form-entry">
+						<label class=>
+							<div>I agree to the terms and conditions
+								<span class="ss-required-asterisk">*</span>
+							</div>
+
+							<div>Sign electronically with your full name. </div>
+						</label>
+					<input type="text" name="signature" value="" class="ss-q-short" id="signature">
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+<input type="hidden" name="draftResponse" value="[]">
 <input type="hidden" name="pageHistory" value="0">
 
 
-<div class="ss-item ss-navigate"><div class="ss-form-entry">
+<div class="form-entry">
 <input type="submit" name="submit" value="Submit" id="ss-submit">
 
-<div class="ss-secondary-text"></div></div></div></form></div>
-<div class="ss-footer"><div class="ss-attribution"></div>
-<div class="ss-legal"><div class="disclaimer-separator"></div>
-<div class="disclaimer"><div class="powered-by-logo"><span class="powered-by-text"></span>
-</div>
-<div class="ss-terms"><span class="disclaimer-msg"></span>
-<br>
-</div></div></div></div>
-</div>
-</form>
-<script type="text/javascript" src="./Harvest Event RSVP Form_files/3090771720-formviewer_prd.js"></script>
-<script type="text/javascript">_initFormViewer();</script></div></body></html>
+</div></form></div>
+</div></div></div>
+
+
+
+
+
--- therealrsvp.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link href="css/therealrsvp.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-
-</body>
-</html>
