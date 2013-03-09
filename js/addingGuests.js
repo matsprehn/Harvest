cd3ef2d02bbdd41c1e11112a7151ad92419797d6
App Jam <appjam@Air06.local>
2013-03-09 15:09:28 -0800

    Forms all submit correctly and some email code

--- js/addingGuests.js
+++ js/addingGuests.js
@@ -13,11 +13,11 @@ $(function(){
     count += 1;
     $('#container').append(
         '<br /><strong>First Name of Guest #' + count + '</strong><br />' 
-        + '<input id="field_' + count + '" firstname="fields[]' + '" type="text" /><br />' 
+        + '<input id="field_' + count + '"name="fields[]' + '" type="text" /><br />' 
         + '<br /><strong>Last Name of Guest #' + count + '</strong><br />' 
-        + '<input id="field_' + count + '" lastname="fields[]' + '" type="text" /><br />' 
+        + '<input id="field_' + count + '" name="fields[]' + '" type="text" /><br />' 
         + '<br /><strong>Email of Guest #' + count + '</strong><br />' 
-        + '<input id="field_' + count + '" email="fields[]' + '" type="text" /><br />' );
+        + '<input id="field_' + count + '" name="fields[]' + '" type="text" /><br />' );
   
   });
 });
--- thankyou.php
+++ thankyou.php
@@ -1,3 +1,111 @@
 <?php
+include 'include/Mailer.php';
+
+echo "lol";
+echo "</br>";
+echo "name is ".$_POST["fname"];
+echo "</br>";
+echo "last name ".$_POST["lname"];
+echo "</br>";
+echo "Email name ".$_POST["email"];
+echo "</br>";
+echo "Phone is ".$_POST["phone"];
+echo "</br>";
+
+
+  $aDoor = $_POST["events"];
+  if(empty($aDoor)) 
+  {
+    echo("You didn't select any buildings.");
+  } 
+  else
+  {
+    $N = count($aDoor);
+ 
+    echo("You selected $N door(s): ");
+    for($i=0; $i < $N; $i++)
+    {
+      echo($aDoor[$i] . " ");
+    }
+  }
+echo "</br>";
+echo "the guest b.s is down here";
+echo "</br>";
+$guest = $_POST["fields"];
+
+echo "";
+if(empty($guest)) 
+  {
+    echo("You didn't select any buildings.");
+  } 
+  else
+  {
+    $N = count($guest);
+ 
+    echo("You selected $N door(s): ");
+    for($i=0; $i < $N; $i++)
+    {
+      echo($guest[$i] . " ");
+    }
+  }
+echo "</br>";
+echo "fuuuu";
+
+echo "</br>";
+echo "THis is my first harvest";
+echo "</br>";
+echo "the value of the first harvest box is ". $_POST["waiver_box"];
+echo "</br>";
+
+$waiver_value = $_POST["waiver_box"];
+
+
+if($waiver_value == "yes"){
+		echo"</br>";
+		echo " The checkbox was selected";
+}else
+{	
+	echo "</br>";
+	echo "The checkbox was not selected";
+}
+
+echo "I'd like to sign up my picture pls pls pls pls";
+
+$picture_value = $_POST["picture_box"];
+
+echo $picture_value;
+
+if($picture_value == "yes"){
+		echo"</br>";
+		echo " The checkbox was selected";
+}else
+{	
+	echo "</br>";
+	echo "The checkbox was not selected";
+}
+
+echo "<br/>";
+echo "the signature is ".$_POST["signature"];
+echo "<br/>"
+
+$mail = new Mailer("jazzy", "jaskaransingh3001@gmail.com", "fuckyou", "SUP :DDD");
+
+/*
+if($mail->sendOK()){
+	echo "IT SENT YAA!";
+}else
+{
+	echo "it didn't send ya noob";
+}
+*/
+
 
 ?>
+
+<html>
+<body>
+
+THANKS FOR SIGNING UP!
+
+</body>	
+</html>
